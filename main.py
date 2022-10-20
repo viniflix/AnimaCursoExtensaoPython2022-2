4d5fa1bd9e277a748106fd5225b8e7feec681d6f
Vinicius Costa <52943476+viniflix@users.noreply.github.com>
2022-10-20 00:52:10 +0000

    primeiros testes

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 #Pojeto Python - Curso de extensão Anima
 
 print("Olá Mundo!")
+
+#Salvando strings em variaveis:
+
+nome = "Vinicius Costa"
+idade = "20"
+
+#Exibir dados das variaveis solicitadas:
+print("Meu nome é: ", nome)
