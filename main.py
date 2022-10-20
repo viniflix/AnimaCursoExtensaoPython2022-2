c58e3c754383efabcb63a55a54ce1beaeaeb60d1
Vinicius Costa <52943476+viniflix@users.noreply.github.com>
2022-10-20 01:18:58 +0000

    Arquivo final da aula 1.

--- main.py
+++ main.py
@@ -2,10 +2,17 @@
 
 print("Olá Mundo!")
 
-#Salvando strings em variaveis:
+#Salvando strings em variáveis:
 
 nome = "Vinicius Costa"
-idade = "20"
+idade = 20
 
-#Exibir dados das variaveis solicitadas:
-print("Meu nome é: ", nome)
+#Exibir dados das variáveis solicitadas:
+print(nome)
+
+#Fase e variável no mesmo print
+print("Minha idade é", idade)
+print(f"Minha idade é {idade}\n")
+
+#Exibir variáveis no meio da frase com .format
+print("Meu nome é {} e tenho {} anos".format(nome, idade))
